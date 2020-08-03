04018eec1028c92be73f86f4ee19048f5ba4c313
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2020-08-03 22:56:30 +0900

    feat: 🎸 Add a background function

---
+++ functions/helloworld/hello_pubsub.go
@@ -0,0 +1,19 @@
+package helloworld
+
+import (
+	"context"
+	"log"
+)
+
+type PubSubMessage struct {
+	Data []byte `json:"data"`
+}
+
+func HelloPubSub(ctx context.Context, m PubSubMessage) error {
+	name := string(m.Data)
+	if name == "" {
+		name = "World"
+	}
+	log.Printf("Hello, %s!", name)
+	return nil
+}
